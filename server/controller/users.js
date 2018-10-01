6ff59eeae5d3a867c9c088653fc06c6de4ad51bb
kosoczky <kosoczky.adam@gmail.com>
2018-10-01 15:43:44 +0200

    hash_password middleware added, hash_randomPassword middleware added, Users controller update

--- server/controller/users.js
+++ server/controller/users.js
@@ -4,14 +4,16 @@ const _ = require('lodash');
 const users = express.Router({mergeParams: true});
 
 const {User} = require('../models/user');
-
+const {hashRandomPassword} = require('../middleware/hash_randomPassword');
 
 //Lvl -> accessLevel Enum:[Admin:0, OfficeAdmin: 1, User:2]
 
 //Create (lvl:0)
-users.post('/', hashPassword, (req, res) => {
+users.post('/', hashRandomPassword, (req, res) => {
     let body = _.pick(req.body, ['name','email', 'password', 'contractId', 'accessLevel', 'group']);
     let user = new User(body);
+    let unHashedRandomPassword = _.pick(req.body, ['unHashedRandomPassword']);
+    sendMail(unHashedRandomPassword);
 
     user.save().then(() => {
         return user.generateAuthToken();
@@ -51,5 +53,25 @@ users.delete('/group/:id', (req, res));
 
 //Delete user (lvl: 0)
 users.delete('/:id', (req, res));
+
+sendMail = function (randomPassword) {
+    var api_key = 'c54eb639082044af33f5d7955c5ceb18-c8e745ec-6e0a36db';
+    var domain = 'sandboxf41e5913ec5d4bfc93fd4096957187aa.mailgun.org';
+    var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
+
+    var data = {
+        from: 'Presence Manager Server <kosoczky.adam@gmail.com>',
+        to: 'kosoczky.adam@gmail.com',
+        subject: 'Flow Academy Regisztrációs Jelszó',
+        text: `Kedves Regisztráló! Első belépéshez való belépési kódod: ${randomPassword}`
+    };
+
+    mailgun.messages().send(data, function (error, body) {
+        if (error) {
+            console.log(error);
+        }
+        console.log(body);
+    });
+};
 
 module.exports = users;
