8042e84a1db8f54b1e0c421d68dddb2c8fd3cf49
dudaseva <dudas.eva.02@gmail.com>
2018-10-02 10:21:17 +0200

    delete/users/group/:id

--- server/controller/users.js
+++ server/controller/users.js
@@ -3,27 +3,27 @@ const _ = require('lodash');
 
 const users = express.Router({ mergeParams: true });
 
-const {User} = require('../models/user');
-const {hashRandomPassword} = require('../middleware/hash_randomPassword');
+const { User } = require('../models/user');
+const { hashRandomPassword } = require('../middleware/hash_randomPassword');
 
 //Lvl -> accessLevel Enum:[Admin:0, OfficeAdmin: 1, User:2]
 
 //Create (lvl:0)
 users.post('/', hashRandomPassword, (req, res) => {
-    let body = _.pick(req.body, ['name','email', 'password', 'contractId', 'accessLevel', 'group']);
-    let user = new User(body);
-    let unHashedRandomPassword = _.pick(req.body, ['unHashedRandomPassword']);
-    sendMail(unHashedRandomPassword, body.email);
+  let body = _.pick(req.body, ['name', 'email', 'password', 'contractId', 'accessLevel', 'group']);
+  let user = new User(body);
+  let unHashedRandomPassword = _.pick(req.body, ['unHashedRandomPassword']);
+  sendMail(unHashedRandomPassword, body.email);
 
-    user.save().then(() => {
-        return user.generateAuthToken();
-    }).then((token) => {
-        res.header('x-auth', token).send({
-            idToken: token,
-            expiresIn: 7200,
-            accessLevel: user.accessLevel
-        });
-    }).catch(e => res.status(400).send(e));
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send({
+      idToken: token,
+      expiresIn: 7200,
+      accessLevel: user.accessLevel
+    });
+  }).catch(e => res.status(400).send(e));
 });
 
 //Read me (lvl:2)
@@ -53,15 +53,15 @@ users.get('/:id', authenticate, (req, res) => {
 
 //Read all (lvl:0)
 users.get('/name', authenticate, (req, res) => {
-	if(req.user.accessLevel !== 0) {
-		return res.status(401).send();
-	}
-	user.find ({
-		_name: req.user._name,
-		_id: req.user._id 
-	})
-	.then(users => res.send(users))
-	.catch(e => res.status(400).send(e));
+  if (req.user.accessLevel !== 0) {
+    return res.status(401).send();
+  }
+  user.find({
+    _name: req.user._name,
+    _id: req.user._id
+  })
+    .then(users => res.send(users))
+    .catch(e => res.status(400).send(e));
 });
 
 
@@ -71,8 +71,8 @@ users.get('/actuals', authenticate, (req, res) => {
   if (req.user.accessLevel !== 0) {
     return res.status(401).send();
   }
-  
-  User.find({'isIn': true}).then((users) => {
+
+  User.find({ 'isIn': true }).then((users) => {
     let actualusers = users.map((user) => { return user.name; });
     res.send({ actualusers });
   }, (e) => {
@@ -84,7 +84,16 @@ users.get('/actuals', authenticate, (req, res) => {
 users.patch('/me', (req, res));
 
 //Delete group (lvl:0)
-users.delete('/group/:id', (req, res));
+users.delete('/users/group/:id', (req, res) => {
+  if (req.user.accessLevel !== 0) {
+    return res.status(400).send();
+  }
+  User.remove({ 'group': req.params.id }).then((user) => {
+    res.send({ user });
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
 
 //Delete user (lvl: 0)
 users.delete('/:id', authenticate, (req, res) => {
@@ -109,23 +118,23 @@ users.delete('/:id', authenticate, (req, res) => {
 });
 
 sendMail = function (randomPassword, email) {
-    var api_key = process.env.API_KEY;
-    var domain = process.env.DOMAIN;
-    var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
+  var api_key = process.env.API_KEY;
+  var domain = process.env.DOMAIN;
+  var mailgun = require('mailgun-js')({ apiKey: api_key, domain: domain });
 
-    var data = {
-        from: 'Presence Manager Server <flowpresencemanager@gmail.com>',
-        to: email,
-        subject: 'Flow Academy Regisztrációs Jelszó',
-        text: `Kedves Regisztráló! Első belépéshez való belépési kódod: ${randomPassword}`
-    };
+  var data = {
+    from: 'Presence Manager Server <flowpresencemanager@gmail.com>',
+    to: email,
+    subject: 'Flow Academy Regisztrációs Jelszó',
+    text: `Kedves Regisztráló! Első belépéshez való belépési kódod: ${randomPassword}`
+  };
 
-    mailgun.messages().send(data, function (error, body) {
-        if (error) {
-            console.log(error);
-        }
-        console.log(body);
-    });
+  mailgun.messages().send(data, function (error, body) {
+    if (error) {
+      console.log(error);
+    }
+    console.log(body);
+  });
 };
 
 module.exports = users;
